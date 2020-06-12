7003f9e6eae8ae47e668daf4efc89469a9697558
JulianCode <hijuliansosa@protonmail.com>
2020-06-12 00:07:46 -0500

    Tipo de dato - Booleanos y condiciones

---
+++ 03-bool-condicion.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool activado = true;
+  
+// activado = !activado;
+  
+  if ( !activado ) {
+    print('El motor esta funcionando');
+  } else {
+    print('El motor esta apagado');
+  }
+  
+  print(activado);
+}
