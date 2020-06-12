af1f51d8df8aad7df58b4d3f46c28bbe51a6de31
JulianCode <hijuliansosa@protonmail.com>
2020-06-12 00:02:10 -0500

    Tipos de datos - Números y Strings

---
+++ 02-tipos-numero-string.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Números
+  int empleados = 10;
+  double pi = 3.14159265;
+  double numero = 1.0; // Si es double, incluso si es 1, debería ser 1.0
+  
+  print(' $empleados - $pi-  $numero');
+}
